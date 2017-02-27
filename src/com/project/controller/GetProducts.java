a66d62f9a9319894b5807e77beb49b760b841e95
ssahu9 <sumit.ps94@gmail.com>
2017-02-27 16:30:09 +0530

    commit web.xml

--- src/com/project/controller/GetProducts.java
+++ src/com/project/controller/GetProducts.java
@@ -1,37 +1,26 @@
 package com.project.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.SQLException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.project.bean.Category;
-import com.project.bl.AdminBL;
 
-public class AddCategory extends HttpServlet {
+public class GetProducts extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		PrintWriter out = null;
-		Category category=new Category();
-		String cName = request.getParameter("cname");
-		category.setCategoryName(cName);
+	
 		
-		AdminBL adminBl= new AdminBL();
-		try {
-			adminBl.addCategory(cName);
-		} catch (ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
-		}
+		
+		
 	}
 
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
