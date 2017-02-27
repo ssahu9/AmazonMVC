6912548c3e19285734c6988aeefcc4727ea11fdf
Ishita Mahindroo <imahin@sapient.com>
2017-02-27 16:12:56 +0530

    remove servlet

---
+++ src/com/project/controller/RemoveProductServlet.java
@@ -0,0 +1,40 @@
+package com.project.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.project.bean.Product;
+import com.project.bl.AdminBL;
+
+public class RemoveProductServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = null;
+		
+		Product product=new Product();
+		int pID = (Integer.parseInt(request.getParameter("pid")));
+		
+		AdminBL adminBl= new AdminBL();
+		try {
+			adminBl.deleteProduct(pID);
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
