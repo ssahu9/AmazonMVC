80a11c48cf3dd72e7c80d7b5122dba42693605db
SAPIENT-vridhi <vridhi.govindp@gmail.com>
2017-02-28 10:35:35 +0530

    admin BL documentation

--- src/com/project/bl/AdminBL.java
+++ src/com/project/bl/AdminBL.java
@@ -19,13 +19,16 @@ import com.project.dao.ProductDaoImpl;
 
 public class AdminBL {
 
-	private AdminDao admin1 = new AdminDaoImpl();
-	private ProductDao product1 = new ProductDaoImpl();
-	private BillDao bill1 = new BillDaoImpl();
-	private CategoryDao category1 = new CategoryDaoImpl();
+	private AdminDao admin1 = new AdminDaoImpl(); 			//new object of AdminDaoImpl created to be used for further functionality
+	private ProductDao product1 = new ProductDaoImpl();		//new object of ProductDaoImpl created to be used for further functionality
+	private BillDao bill1 = new BillDaoImpl();				//new object of BillDaoImpl created to be used for further functionality
+	private CategoryDao category1 = new CategoryDaoImpl();	//new object of CategoryDaoImpl created to be used for further functionality
 
+	
+	
+	//to check the email validation of admin using adminDaoImpl
 	public boolean signIn(String email, String password) throws ClassNotFoundException, SQLException {
-		// check email
+		
 		String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
 		java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
 		java.util.regex.Matcher m = p.matcher(email);
@@ -37,7 +40,8 @@ public class AdminBL {
 		return false;
 	}
 
-	// check password length
+	
+	// check password length of admin using adminDaoImpl
 	public boolean updatePassword(Admin admin) throws SQLException, ClassNotFoundException {
 		if ((admin.getPassword().length() < 8)) {
 			System.out.println("Password needs to be atleast 8 character longer");
@@ -46,36 +50,46 @@ public class AdminBL {
 		return admin1.update(admin);
 	}
 
+	
+	//function to call addCategory() method from CategoryDaoImpl
 	public boolean addCategory(String categoryname) throws SQLException, ClassNotFoundException {
 		return category1.insertCategory(categoryname);
 	}
 
+	//function to call viewCategory() method from CategoryDaoImpl
 	public List<Category> viewCategory() throws ClassNotFoundException, SQLException {
 		return category1.viewCategory();
 
 	}
 
+	
+	//function to call deleteCategory() method from CategoryDaoImpl
 	public boolean deleteCategory(String categoryname) throws SQLException, ClassNotFoundException {
 		return category1.deleteCategory(categoryname);
 	}
 
+	//function to call addProduct() method from ProductDaoImpl
 	public boolean addProduct(Product product) throws SQLException, ClassNotFoundException {
 		return product1.addProduct(product);
 	}
 
+	//function to call viewProduct() method from ProductDaoImpl
 	public List<Product> viewProduct(String pcategory) throws ClassNotFoundException, SQLException {
 		return product1.viewProduct(pcategory);
 	}
 
+	//function to call updateProduct() method from ProductDaoImpl
 	public boolean updateProduct(Product product) throws ClassNotFoundException, SQLException {
 		return product1.updateProduct(product);
 
 	}
 
+	//function to call deleteProduct() method from ProductDaoImpl
 	public Boolean deleteProduct(int pid) throws ClassNotFoundException, SQLException {
 		return product1.deleteProduct(pid);
 	}
 
+	//function to call getAllBillDetails() method from BillDaoImpl
 	public List<BillDetails> getAllBillDetails() throws SQLException, ClassNotFoundException {
 		return bill1.getAllBillDetails();
 	}
