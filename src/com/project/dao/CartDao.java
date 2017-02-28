f36c5694b5a89446a0a84e8f99f376f5e0643409
SAPIENT-vridhi <vridhi.govindp@gmail.com>
2017-02-28 12:34:21 +0530

    DaoInterface doc

--- src/com/project/dao/CartDao.java
+++ src/com/project/dao/CartDao.java
@@ -6,9 +6,13 @@ import com.project.bean.Cart;
 import com.project.bean.CartDetails;
 
 public interface CartDao {
+	
+	//addToCart function linked to CartDaoImpl
 	boolean addToCart(List<Cart> cartList) throws ClassNotFoundException, SQLException;
 
+	//removeFromCart function linked to CartDaoImpl
 	boolean removeFromCart(int productId, int customerIdt) throws ClassNotFoundException, SQLException;
 
+	//viewCart function linked to CartDaoImpl
 	List<CartDetails> viewCart(int Customer_id) throws SQLException, ClassNotFoundException;
 }
