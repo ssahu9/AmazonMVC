478d5193da8c17cc3e48cbdf7926589787584c31
SAPIENT-vridhi <vridhi.govindp@gmail.com>
2017-02-28 11:26:54 +0530

    customer bl docu

--- src/com/project/bl/CustomerBl.java
+++ src/com/project/bl/CustomerBl.java
@@ -23,12 +23,14 @@ import com.project.dao.ProductDaoImpl;
 
 public class CustomerBl {
 
-	private CustomerDao user = new CustomerDaoImpl();
-	private CartDao cart = new CartDaoImpl();
-	private CategoryDao category1 = new CategoryDaoImpl();
-	private BillDao bill = new BillDaoImpl();
-	private ProductDao product = new ProductDaoImpl();
+	private CustomerDao user = new CustomerDaoImpl();			//new object created of CustomerDaoImpl
+	private CartDao cart = new CartDaoImpl();					//new object created of CartDaoImpl
+	private CategoryDao category1 = new CategoryDaoImpl();		//new object created of CategoryDaoImpl
+	private BillDao bill = new BillDaoImpl();					//new object created of BillDaoImpl
+	private ProductDao product = new ProductDaoImpl();			//new object created of ProductDaoImpl
 
+	
+	//email validation for customer using signIn method in CustomerDaoImpl
 	public Customer signIn(String email, String password) throws ClassNotFoundException, SQLException {
 		String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
 		java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
@@ -42,6 +44,8 @@ public class CustomerBl {
 
 	}
 
+	
+	//password length check and email validation for customer signUp method in CustomerDaoImpl
 	public int signUp(Customer customer) throws ClassNotFoundException, SQLException {
 
 		if ((customer.getPassword().length() < 8)) {
@@ -63,6 +67,8 @@ public class CustomerBl {
 		return user.insert(customer);
 	}
 
+	
+	//new function of updateDetails() method in CustomerDaoImpl
 	public boolean updateDetails(Customer customer) throws ClassNotFoundException, SQLException {
 
 		if ((customer.getPassword().length() < 8)) {
@@ -83,39 +89,57 @@ public class CustomerBl {
 		return user.update(customer);
 	}
 
+	
+	//new function of viewCart() method in CartDaoImpl
 	public List<CartDetails> viewCart(int customerId) throws ClassNotFoundException, SQLException {
 		return cart.viewCart(customerId);
 	}
 
+	
+	//new function of addToCart() method in CartDaoImpl
 	public boolean addToCart(List<Cart> cartList) throws ClassNotFoundException, SQLException {
 
 		return cart.addToCart(cartList);
 	}
 
+	
+	//new function of removeFromCart() method in CartDaoImpl
 	public boolean removeFromCart(int productId, int customerId) throws ClassNotFoundException, SQLException {
 		return cart.removeFromCart(productId, customerId);
 	}
 
+	
+	//new function of viewCategory() method in CategoryDaoImpl
 	public List<Category> viewCategory() throws ClassNotFoundException, SQLException {
 		return category1.viewCategory();
 	}
 
+	
+	//new function of generateBill() method in BillDaoImpl
 	public Bill generateBill(int customerId) throws ClassNotFoundException, SQLException {
 		return bill.generateBill(customerId);
 	}
 
+	
+	//new function of getBillDetails() method in BillDaoImpl
 	public List<BillDetails> getBillDetails(int customerId) throws ClassNotFoundException, SQLException {
 		return bill.getBillDetails(customerId);
 	}
 
+	
+	//new function of getCurrentDetails() method in BillDaoImpl
 	public List<BillDetails> getCurrentBill(Bill bill1) throws ClassNotFoundException, SQLException {
 		return bill.getCurrentBill(bill1);
 	}
 
+	
+	//new function of viewProduct() method in ProductDaoImpl
 	public List<Product> viewProduct(String pcategory) throws ClassNotFoundException, SQLException {
 		return product.viewProduct(pcategory);
 	}
 
+	
+	//new function of searchProductByNames() method in ProductDaoImpl
 	public Product searchProductByName(String productName) throws ClassNotFoundException, SQLException {
 		return product.searchProductByName(productName);
 	}
