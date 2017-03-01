10984635fce330c8a08cdcf94db1671cc2cf8099
Kshitij Jain <kshitij.jain7@gmail.com>
2017-03-01 10:10:36 +0530

    Forgot password JSP and servlet

---
+++ src/com/project/controller/ForgotPasswordServlet.java
@@ -0,0 +1,30 @@
+package com.project.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ForgotPasswordServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		if(request.getParameter("pwd").equals(request.getParameter("rpwd")))
+			request.getRequestDispatcher("index.jsp").forward(request, response);
+		else {
+			out.println("Password change failed!");
+			request.getRequestDispatcher("ForgotPassword.jsp").include(request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
