aec7bd19a7bc074de5af89d364cb637c885e2260
Kshitij Jain <kshitij.jain7@gmail.com>
2017-03-01 10:37:02 +0530

    Logout session parameterized Forgot password session added

--- src/com/project/controller/ForgotPasswordServlet.java
+++ src/com/project/controller/ForgotPasswordServlet.java
@@ -2,11 +2,17 @@ package com.project.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.project.bean.Customer;
+import com.project.bl.CustomerBl;
+import com.sun.xml.internal.ws.resources.HttpserverMessages;
 
 public class ForgotPasswordServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -15,8 +21,19 @@ public class ForgotPasswordServlet extends HttpServlet {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		
-		if(request.getParameter("pwd").equals(request.getParameter("rpwd")))
+		CustomerBl customerBL = new CustomerBl();
+		
+		if(request.getParameter("pwd").equals(request.getParameter("rpwd"))) {
+			HttpSession session = request.getSession(false);
+			Customer customer = (Customer) session.getAttribute("customerObject");
+			customer.setPassword(request.getParameter("pwd"));
+			try {
+				customerBL.updateDetails(customer);
+			} catch (ClassNotFoundException | SQLException e) {
+				e.printStackTrace();
+			}
 			request.getRequestDispatcher("index.jsp").forward(request, response);
+		}
 		else {
 			out.println("Password change failed!");
 			request.getRequestDispatcher("ForgotPassword.jsp").include(request, response);
